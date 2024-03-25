3be18d09ad12b6d57f57f5fc7af0810028e97deb
Sarraa Muhammad <sarraa.khalil01@eng-st.cu.edu.eg>
2024-03-25 23:52:02 +0200

    Added Activity Tests in Notifications Settings Page

--- src/main/java/NotificationsSettingsPage.java
+++ src/main/java/NotificationsSettingsPage.java
@@ -12,10 +12,39 @@ public class NotificationsSettingsPage extends PageBase{
     By chatMessagesCheckboxLocator = By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div[1]/span[2]/div/div[2]/div/button");
     By chatRequestsCheckboxLocator = By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div[1]/span[3]/div/div[2]/div/button");
 
+    // Activity Notifications
+    By usernameMentionsCheckboxLocator = By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div[2]/span[1]/div/div[2]/div/button");
+    By commentsCheckboxLocator = By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div[2]/span[2]/div/div[2]/div/button");
+    By upvotesOnPostsCheckboxLocator = By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div[2]/span[3]/div/div[2]/div/button");
+    By upvotesOnCommentsCheckboxLocator = By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div[2]/span[4]/div/div[2]/div/button");
+    By RepliesToCommentsCheckboxLocator = By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div[2]/span[5]/div/div[2]/div/button");
+    By activityOnCommentCheckboxLocator = By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div[2]/span[6]/div/div[2]/div/button");
+    By activityOnChatPostsCheckboxLocator = By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div[2]/span[7]/div/div[2]/div/button");
+    By newFollowersCheckboxLocator = By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div[2]/span[8]/div/div[2]/div/button");
+    By awardsReceivedCheckboxLocator = By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div[2]/span[9]/div/div[2]/div/button");
+    By postsFollowedCheckboxLocator = By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div[2]/span[10]/div/div[2]/div/button");
+    By commentsFollowedCheckboxLocator = By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div[2]/span[11]/div/div[2]/div/button");
+
+
     WebElement notificationsTab;
+    // Messages Notifications Elements
     WebElement privateMessagesCheckbox;
     WebElement chatMessagesCheckbox;
     WebElement chatRequestsCheckbox;
+
+    // Activity Notifications Elements
+    WebElement usernameMentionsCheckbox;
+    WebElement commentsCheckbox;
+    WebElement upvotesOnPostsCheckbox;
+    WebElement upvotesOnCommentsCheckbox;
+    WebElement RepliesToCommentsCheckbox;
+    WebElement activityOnCommentCheckbox;
+    WebElement activityOnChatPostsCheckbox;
+    WebElement newFollowersCheckbox;
+    WebElement awardsReceivedCheckbox;
+    WebElement postsFollowedCheckbox;
+    WebElement commentsFollowedCheckbox;
+
 
     public void openNotificationsTab()
     {
@@ -34,4 +63,72 @@ public class NotificationsSettingsPage extends PageBase{
         Clicking(chatRequestsCheckbox);
         openNotificationsTab();
     }
+
+    public void changeMentionsNotifications()
+    {
+        usernameMentionsCheckbox = driver.findElement(usernameMentionsCheckboxLocator);
+        Clicking(usernameMentionsCheckbox);
+    }
+
+    public void changeCommentsNotifications()
+    {
+        commentsCheckbox = driver.findElement(commentsCheckboxLocator);
+        Clicking(commentsCheckbox);
+    }
+
+    public void changeUpvotesOnPostsNotifications()
+    {
+        upvotesOnPostsCheckbox = driver.findElement(upvotesOnPostsCheckboxLocator);
+        Clicking(upvotesOnPostsCheckbox);
+    }
+
+    public void changeUpvotesOnCommentsNotifications()
+    {
+        upvotesOnCommentsCheckbox = driver.findElement(upvotesOnCommentsCheckboxLocator);
+        Clicking(upvotesOnCommentsCheckbox);
+    }
+
+    public void changeRepliesToCommentsNotifications()
+    {
+        RepliesToCommentsCheckbox = driver.findElement(RepliesToCommentsCheckboxLocator);
+        Clicking(RepliesToCommentsCheckbox);
+    }
+
+    public void changeActivityOnCommentNotifications()
+    {
+        activityOnCommentCheckbox = driver.findElement(activityOnCommentCheckboxLocator);
+        Clicking(activityOnCommentCheckbox);
+    }
+
+    public void changeActivityOnChatPostsNotifications()
+    {
+        activityOnChatPostsCheckbox = driver.findElement(activityOnChatPostsCheckboxLocator);
+        Clicking(activityOnChatPostsCheckbox);
+    }
+
+    public void changeNewFollowersNotifications()
+    {
+        newFollowersCheckbox = driver.findElement(newFollowersCheckboxLocator);
+        Clicking(newFollowersCheckbox);
+    }
+
+    public void changeAwardsReceivedNotifications()
+    {
+        awardsReceivedCheckbox = driver.findElement(awardsReceivedCheckboxLocator);
+        Clicking(awardsReceivedCheckbox);
+    }
+
+    public void changePostsFollowedNotifications()
+    {
+        postsFollowedCheckbox = driver.findElement(postsFollowedCheckboxLocator);
+        Clicking(postsFollowedCheckbox);
+    }
+
+    public void changeCommentsFollowedNotifications()
+    {
+        commentsFollowedCheckbox = driver.findElement(commentsFollowedCheckboxLocator);
+        Clicking(commentsFollowedCheckbox);
+    }
+
+
 }
--- src/test/java/NotificationsSettingsPageTest.java
+++ src/test/java/NotificationsSettingsPageTest.java
@@ -27,4 +27,38 @@ public class NotificationsSettingsPageTest extends TestBase {
         Assert.assertTrue(driver.findElement(notificationsSettingsPage.privateMessagesCheckboxLocator).isDisplayed());
         implicitWait(5);
     }
+
+    @Test
+    public void changeActivityNotificationsTest() throws InterruptedException {
+        loginPage.login("Ill_Initial_3945", "/m4!2QHj!saysm4");
+        implicitWait(5);
+        accountSettingsPage.openSettings();
+        implicitWait(5);
+        notificationsSettingsPage.openNotificationsTab();
+        implicitWait(5);
+        notificationsSettingsPage.changeMentionsNotifications();
+        Thread.sleep(10000);
+        notificationsSettingsPage.changeCommentsNotifications();
+        Thread.sleep(10000);
+        notificationsSettingsPage.changeUpvotesOnPostsNotifications();
+        Thread.sleep(10000);
+        notificationsSettingsPage.changeUpvotesOnCommentsNotifications();
+        Thread.sleep(10000);
+        notificationsSettingsPage.changeRepliesToCommentsNotifications();
+        Thread.sleep(10000);
+        notificationsSettingsPage.changeActivityOnCommentNotifications();
+        Thread.sleep(10000);
+        notificationsSettingsPage.changeActivityOnChatPostsNotifications();
+        Thread.sleep(10000);
+        notificationsSettingsPage.changeNewFollowersNotifications();
+        Thread.sleep(10000);
+        notificationsSettingsPage.changeAwardsReceivedNotifications();
+        Thread.sleep(10000);
+        notificationsSettingsPage.changePostsFollowedNotifications();
+        Thread.sleep(10000);
+        notificationsSettingsPage.changeCommentsFollowedNotifications();
+        Thread.sleep(10000);
+        Assert.assertTrue(driver.findElement(notificationsSettingsPage.usernameMentionsCheckboxLocator).isDisplayed());
+        implicitWait(5);
+    }
 }
